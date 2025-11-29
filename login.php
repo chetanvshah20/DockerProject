2d8cffa7777a40a0a68e86805ac96bd43f0f831d
Chetan V. Shah <79932411+chetanvshah20@users.noreply.github.com>
2025-11-29 09:46:40 +0530

    Add Login Files

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+$servername = "172.31.76.201";  // Remote MySQL server IP
+$username   = "root";           // MySQL user
+$password   = "Admin@123";               // MySQL password
+$dbname     = "payroll";        // DB name
+
+// Create MySQL Connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check Connection
+if ($conn->connect_error) {
+    die("Connection Failed: " . $conn->connect_error);
+}
+
+// Read Form Data
+$user = $_POST['username'];
+$pass = $_POST['password'];
+
+echo "Username: " . $user . "<br>";
+echo "Password: " . $pass . "<br>";
+?>
