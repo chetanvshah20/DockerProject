f3ad2b68ac80e0942a4139e2e7e6d6ee57bfe9b1
Chetan V. Shah <79932411+chetanvshah20@users.noreply.github.com>
2025-12-28 13:36:12 +0530

    Update MySQL server IP in login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ session_start();
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
-$servername = "172.31.9.48";  // Remote MySQL server IP
+$servername = "172.31.15.77";  // Remote MySQL server IP
 $username   = "root";           // MySQL user
 $password   = "Admin@123";               // MySQL password
 $dbname     = "payroll";        // DB name
@@ -24,4 +24,5 @@ $pass = $_POST['password'];
 echo "Username: " . $user . "<br>";
 echo "Password: " . $pass . "<br>";
 ?>
+
 
